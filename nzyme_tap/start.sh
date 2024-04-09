d7b05bbaba37aadac6c8e9f05046635034cd7e2b
rasheedsaeed <rasheedsaeed@protonmail.com>
2024-04-10 00:00:56 +0100

    Init

--- nzyme_tap/start.sh
+++ nzyme_tap/start.sh
@@ -9,18 +9,4 @@ echo "---------- Starting nzyme tap -----------"
 config_file=/etc/nzyme/nzyme-tap.conf
 envsubst < /etc/nzyme/nzyme-tap.conf.template > $config_file
 
-# # Check if the montap interface does not exist
-# if ! ip link show montap > /dev/null 2>&1; then
-#     # If montap doesn't exist, proceed with the setup
-#     ip link set dev wlan1 down
-#     ip link set wlan1 name montap
-#     iw dev montap interface add wlan1 type managed
-
-#     ip link set dev montap down
-#     iw dev montap set type monitor
-#     ip link set dev montap up
-# else
-#     echo "The montap interface already exists. No changes made."
-# fi
-
 /usr/bin/nzyme-tap --configuration-file $config_file --log-level $LOG_LEVEL
