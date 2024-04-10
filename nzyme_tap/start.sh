6c51f2e20def3a940cf93ac544125a19c7718c2a
rasheedsaeed <rasheedsaeed@protonmail.com>
2024-04-10 17:32:47 +0100

    Removed unusued installs from node and tap dockerfiles

--- nzyme_tap/start.sh
+++ nzyme_tap/start.sh
@@ -9,4 +9,6 @@ echo "---------- Starting nzyme tap -----------"
 config_file=/etc/nzyme/nzyme-tap.conf
 envsubst < /etc/nzyme/nzyme-tap.conf.template > $config_file
 
+cat $config_file
+
 /usr/bin/nzyme-tap --configuration-file $config_file --log-level $LOG_LEVEL
