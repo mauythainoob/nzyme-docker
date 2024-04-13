081468c1c965bed5b318fa67de475091af9178f4
rasheedsaeed <rasheedsaeed@protonmail.com>
2024-04-13 22:01:03 +0100

    Nzyme user created for node and tap... improves container security

--- nzyme_node/start.sh
+++ nzyme_node/start.sh
@@ -6,4 +6,5 @@ envsubst < /etc/nzyme/nzyme.conf.template > /etc/nzyme/nzyme.conf
 # Configuration for the heap (see https://docs.nzyme.org/getting-started/installation/ubuntu-server-2204/#configure-nzymeconf)
 echo 'NZYME_JAVA_OPTS="-Xms${INITIAL_HEAP_SIZE_IN_GB}g -Xmx${MAX_HEAP_SIZE_IN_GB}g -XX:-OmitStackTraceInFastThrow"' > /etc/default/nzyme
 
+# Use postgres-client to request the keys from the database and then write it
 /usr/share/nzyme/bin/nzyme 
